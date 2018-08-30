21fe64eaaefcb11fa7d088cfabc0f7fe9f3d81a1
Przemysław Kozłowski <support@kozioldeveloper.pl>
2018-08-30 14:33:56 +0200

    .

---
+++ config.sample.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  "siteKey": "someKey",
+  "mssql": {
+    "user": 'sa',
+    "password": 'Wapro3000',
+    "server": 'serwer',
+    "database": 'database',
+    "pool": {
+      "max": 10,
+      "min": 0,
+      "idleTimeoutMillis": 30000
+    },
+    "options": {
+      "encrypt": false
+    }
+  }
+};
+
+module.exports.logs = function () {
+  Array.prototype.unshift.call(arguments, '[' + new Date().toLocaleString() + ']');
+  return console.log.apply(this, arguments);
+}
